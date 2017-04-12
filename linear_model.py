e953ae5656842e2c634c10cc8ee0c399c6fb3cfc
Chris Ying <chrsyng@gmail.com>
2017-04-12 02:42:31 -0400

    fix outbias bug, fix target slightly)

--- linear_model.py
+++ linear_model.py
@@ -47,7 +47,7 @@ class LinearModel:
 
         # TODO: for actor-critic, pred should be predicted from a separate critic network instead
 
-        target = (action - pred) * reward / var
+        target = (action - pred) * reward / (2.0 * var)
         _, loss, summary = self.sess.run([self.train_op, self.loss, self.summary],
                 feed_dict={self.input_state: state, self.target: target})
 
@@ -89,7 +89,7 @@ class LinearModel:
         out_weight = tf.Variable(tf.truncated_normal([64, self.output_size]), name='out_weight')
         out_bias = tf.Variable(tf.constant(0.1, shape=[self.output_size]), name='out_bias')
         self.out = tf.matmul(self.fc2, out_weight)
-        self.out = tf.nn.bias_add(self.out, self.out_bias)
+        self.out = tf.nn.bias_add(self.out, out_bias)
         tf.summary.histogram('output_values', self.out)
 
         self.target = tf.placeholder(tf.float32, [None, self.output_size])
