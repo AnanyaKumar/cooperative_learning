dea788e29a12d321c5d2d1c50b33468390969f90
Edgar Chen <eggylv999@gmail.com>
2017-04-30 22:29:42 -0400

    remove epsilon

--- car.py
+++ car.py
@@ -38,9 +38,7 @@ class Car:
         new_vel_x = self.vel_x + acc_x * time
         new_vel_y = self.vel_y + acc_y * time
 
-        EPSILON = 1e-5      # epsilon to prevent div by 0 issues
-
-        if new_vel_x <= max_vel + EPSILON:
+        if new_vel_x < max_vel:
             self.pos_x = self.pos_x + self.vel_x * time + 0.5 * acc_x * (time ** 2)
             self.vel_x = new_vel_x
         else:
@@ -48,7 +46,7 @@ class Car:
             self.pos_x = self.pos_x + self.vel_x * dt + 0.5 * acc_x * (dt ** 2) + max_vel * (time - dt)
             self.vel_x = max_vel
 
-        if new_vel_y <= max_vel + EPSILON:
+        if new_vel_y < max_vel:
             self.pos_y = self.pos_y + self.vel_y * time + 0.5 * acc_y * (time ** 2)
             self.vel_y = new_vel_y
         else:
